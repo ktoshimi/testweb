650c7d212d8f9418796174444e721fe8217d4ed9
toshi <1105.toshi.k@gmail.com>
2018-05-12 17:42:19 +0900

    3度目の正直

---
+++ livmo/bokuno/php/instagram.php
@@ -0,0 +1,11 @@
+<?php
+//POSTリクエストの場合のみ受付
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    //アクセストークン
+    $access_token = "195935951.6cda97c.fd05fcab3a284895a578691440f0dbe7"; //取得したアクセストークンを設置
+    //JSONデータを取得して出力
+    echo @file_get_contents("https://api.instagram.com/v1/users/self/media/recent/?access_token={$access_token}");
+    //終了
+    exit;
+}
+?>
